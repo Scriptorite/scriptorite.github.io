5812474c1c673f0c8d8ece4cce3670ca64640245
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-05-21 08:36:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,68 +1,41 @@
-// This is the "Offline copy of pages" service worker
-Notification.requestPermission();
-const CACHE = "pwabuilder-offline";
 
+// service-worker.js
 
+const cacheName = 'my-cache';
+const offlinePage = '/offline.html';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-// Check to make sure Sync is supported.
-if ('serviceWorker' in navigator && 'SyncManager' in window) {
-
-  // Get our service worker registration.
-  const registration = await navigator.serviceWorker.registration;
-
-  try {
-    // This is where we request our sync. 
-    // We give it a "tag" to allow for differing sync behavior.
-    await registration.sync.register('database-sync');
-
-  } catch {
-    console.log("Background Sync failed.")
-  }
-}
-
-
-// Add an event listener for the `sync` event in your service worker.
-self.addEventListener('sync', event => {
-
-  // Check for correct tag on the sync event.
-  if (event.tag === 'database-sync') {
-
-    // Execute the desired behavior with waitUntil().
-    event.waitUntil(
-
-      // This is just a hypothetical function for the behavior we desire.
-      pushLocalDataToDatabase();
-    );
-    }
-});
-
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
-
-self.addEventListener('push', (event) => {
+// Install event
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    self.registration.showNotification('Notification Title', {
-      body: 'Notification Body Text',
-      icon: '/assets/favicon-1248x1248.png',
-    });
+    caches.open(cacheName)
+      .then((cache) => {
+        return cache.addAll([
+          '/', // Cache the root page
+          '/index.html', // Cache the index page
+          '*.html', // Cache all HTML pages
+          offlinePage, // Cache the offline page
+        ]);
+      })
   );
 });
 
-self.addEventListener('notificationclick', (event) => {
-    event.notification.close(); 
-    var fullPath = self.location.origin + event.notification.data.path; 
-    clients.openWindow(fullPath); 
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then((networkResponse) => {
+            const cache = caches.open(cacheName);
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          })
+          .catch(() => {
+            return caches.match(offlinePage); // Return the cached offline page
+          });
+      })
+  );
 });
