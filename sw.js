7e37a9517f308a4605dfb247e1a33fc8c4b5c519
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-05-26 10:57:19 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,22 @@
 
 // service-worker.js
 
-const cacheName = 'my-cache';
-const offlinePage = '/offline.html';
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
 
-// Install event
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName)
-      .then((cache) => {
-        return cache.addAll([
-          '/', // Cache the root page
-          '/index.html', // Cache the index page
-          '*.html', // Cache all HTML pages
-          offlinePage, // Cache the offline page
-        ]);
-      })
-  );
-});
-
-// Fetch event
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request)
-          .then((networkResponse) => {
-            const cache = caches.open(cacheName);
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          })
-          .catch(() => {
-            return caches.match(offlinePage); // Return the cached offline page
-          });
-      })
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      *.html
+      *.scss
+      *.md
+      *.jpg
+      *.png
+      *.js
+    ]),
   );
 });
